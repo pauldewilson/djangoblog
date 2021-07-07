cb1d5a7bd43173c1f571cdb035c6f248f5678f12
Paul <pauldewilson@gmail.com>
2021-07-07 23:06:21 +0100

    pagination added to blog list

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,30 @@
+from django.core.paginator import (
+    Paginator, EmptyPage, PageNotAnInteger
+)
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
+
+
 def post_list(request):
     """
     View that returns all published posts
     """
-    posts = Post.published.all()
+    object_list = Post.published.all() # all published posts
+    paginator = Paginator(object_list, 3) # 3 posts per page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # if page not an int then return first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range then return last page
+        posts = paginator.page(paginator.num_pages)
+
     return render(request, 'blog/post/list.html', {'posts': posts})
+
 
 def post_detail(request, year, month, day, post):
     """
