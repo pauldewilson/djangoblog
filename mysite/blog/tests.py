a3c72635c25c7b670aefbec0838cf2d5419e38d3
Paul <pauldewilson@gmail.com>
2021-07-07 22:50:04 +0100

    Templates created and working

--- mysite/blog/tests.py
+++ mysite/blog/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
 
-# Create your tests here.
+def create_test_user(test_username):
+    """
+    Creates a test user
+    """
+    return User.objects.create(username=test_username)
+
+class PostTestCases(TestCase):
+
+
+    def test_can_create_post_object(self):
+
+        test_user = create_test_user('TEST USER')
+
+        new_post = Post(title='test create', author=test_user)
+        new_post.save()
+
+        response_post = Post.objects.get(id=1)
+
+        self.assertEqual(response_post, new_post)
+    
+    def test_created_post_title_returned_correctly(self):
+
+        test_new_title = 'ABC123'
+
+        test_user = create_test_user('TEST USER')
+
+        new_post = Post(title=test_new_title, author=test_user)
+        new_post.save()
+
+        response_post = Post.objects.get(id=1)
+
+        self.assertEqual(test_new_title, response_post.title)
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+    """
+    View that returns all published posts
+    """
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    """
+    View that shows individual post
+    """
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        status='published',
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
+    return render(request, 'blog/post/detail.html', {'post': post})
