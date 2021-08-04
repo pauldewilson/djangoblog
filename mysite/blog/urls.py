6137c93b9ec7658d25b5fc2e5439a35f2594b1f3
Paul <pauldewilson@gmail.com>
2021-08-05 00:12:19 +0100

    basic tags functionality finished

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='post_list'),
+    # path('', views.PostListView.as_view(), name='post_list'), # old class based view before creating tag view (post_list)
+    path('', views.post_list, name='post_list'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path(
         '<int:year>/<int:month>/<int:day>/<slug:post>/',
         views.post_detail,
