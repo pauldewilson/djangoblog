19198904972e76147f5d2361f163796d9e5a29b8
Paul <pauldewilson@gmail.com>
2021-08-07 14:00:58 +0100

    added similar posts functionality

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.models import User
 from django.views.generic import ListView
+from django.db.models import Count
 from django.urls import reverse
 from taggit.models import Tag
 from .models import Post, Comment
@@ -49,11 +50,16 @@ def post_detail(request, year, month, day, post):
         # GET request (page load)
         # instantiate blank comment form
         comment_form = CommentForm()
+    # list of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
     return render(request, 'blog/post/detail.html', {
         'post': post,
         'comments': comments,
         'new_comment': new_comment,
         'comment_form': comment_form,
+        'similar_posts': similar_posts,
     })
 
 
