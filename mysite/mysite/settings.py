9ae58c63d7ba9dc5d06b4e3770fa3f071e1f7550
Paul <pauldewilson@gmail.com>
2021-08-09 23:00:49 +0100

    sitemap added

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'django.contrib.sites', # commented out otherwise sitemap does not work
+    # though this means that domain management is not available in admin
+    'django.contrib.sitemaps',
     'blog.apps.BlogConfig',
     'taggit',
 ]
